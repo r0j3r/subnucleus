59809d42faf91bf524d82e97de6812fbd899298a
rogelio <rogelio.serrano@gmail.com>
2014-11-12 19:14:26 +0000

    use two arrays for holding the ticks count called expired and active. they are switched when the active array is all 0.

--- kern/cpu.h
+++ kern/cpu.h
@@ -2,7 +2,8 @@
 struct cpu
 {
     unsigned int q_env_id[1 << 7];
-    unsigned int q_ticks[1 << 7];
+    unsigned int * q_active;
+    unsigned int * q_expired; 
     unsigned short q_no[1 << 7];
     unsigned long regs[32]; 
 };
--- kern/sched.c
+++ kern/sched.c
@@ -11,16 +11,13 @@ init_quantum_vec(void) {
     for (int i = 0; i < NUM_CPU; i++) {
         struct cpu * c = &cpu_ctx[i];
         for(int j = 0; j < (1 << 7); j++) {
-            c->q_envid = 0;
-            c->q_ticks = 0;
-            c->q_no = j; 
+            c->q_no[j] = j; 
         }
     }
 }
 
 int
-init_queue(struct env ** q)
-{
+init_queue(struct env ** q) {
     *q = malloc(sizeof*(*q)));
     memset(*q, 0, sizeof*(*q)));
     *q->next = *q;
@@ -28,24 +25,42 @@ init_queue(struct env ** q)
 }
 
 int
-insert_env(struct env * e, struct env * q)
-{
+insert_env(struct env * e, struct env * q) {
     e->next = q->next;
     q->next = e;
     return 0;
 }
 
 int
-syscall_yield()
-{
+syscall_yield() {
 
 }
 
 int
-sched_next() {
+sched_next(void) {
     cur_quant = (cur_quant + 1) & ((1 << 7) - 1);
     if (!cur_quant) {
-        more_ticks();
+        a = active;
+        active = expired;
+        expired = a;
         cur_quant = 1; 
     }
+}
+
+void
+sched_intr(int trapno) {
+    now = get_time();
+    ev = dequeue();
+    while(now > ev->expiry) {
+        if (ev->timer) {
+            process_timer(now);
+        } else if (ev->quantum) {
+            active[cur_quant] = 0;
+            expired[cur_quant] = 8;
+            sched_next();
+            enqueue_quantum(cur_quant);
+        } 
+    }
+    get_next_timeout();
+    intr_return();
 }
