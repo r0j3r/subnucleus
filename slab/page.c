8dfcc296ef4ba337ede6c1f65d58826e69693e8a
rogelio <rogelio.serrano@gmail.com>
2014-11-16 03:31:39 +0000

    use unrolled list for cache control structs

--- slab/page.c
+++ slab/page.c
@@ -1,7 +1,50 @@
-struct page
-{
-    
+struct kmem_cache;
+
+struct slab {
+    struct slab * next;
+    struct kmem_cache * c;
+    unsigned int inuse;
+    unsigned char * buffers;
+    unsigned int buf_cnt;
+    unsigned char bmap[];
+}; 
+
+struct unrolled_list {
+    struct unrolled_list * next;
+    int num;
+    int elem_size;
+    int maxnum;
+    unsigned char elem[];
 };
+
+struct kmem_cache {
+    unsigned int size;
+    unsigned int aligned size;
+    struct slab * empty;
+    struct slab * partial;
+    struct slab * full;
+};
+
+struct unrolled_list * cache_list;
+ 
+
+struct page {
+    struct slab * s;
+};
+
+int
+init_cache() {
+    int size = sizeof(struct kmem_cache);
+    if (size < (4096/8)) {
+        pages = 1;
+        cache_list = get_page(pages);
+    } else {
+        pages = (((size * 8) + 4095) & ~4095) / 4096;
+        cache_list = get_page(pages);
+    }
+    memset(cache_list, 0, 4096 * pages);
+       
+}
 
 int
 get_pages()
