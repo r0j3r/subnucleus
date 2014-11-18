66b4c35a7c9902e5ad76883d154aea7a4bae26eb
rogelio <rogelio.serrano@gmail.com>
2014-11-18 23:27:31 +0000

    initial implementation of binary search routines

---
+++ kern/bsearch.c
@@ -0,0 +1,23 @@
+int bsearch(int A[], int key, int imin, int imax)
+{
+  // test if array is empty
+  if (imax < imin)
+    // set is empty, so return value showing not found
+    return KEY_NOT_FOUND;
+  else
+    {
+      // calculate midpoint to cut set in half
+      int imid = midpoint(imin, imax);
+ 
+      // three-way comparison
+      if (A[imid] > key)
+        // key is in lower subset
+        return binary_search(A, key, imin, imid-1);
+      else if (A[imid] < key)
+        // key is in upper subset
+        return binary_search(A, key, imid+1, imax);
+      else
+        // key has been found
+        return imid;
+    }
+}
